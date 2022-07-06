a5857748c59f302d078100f04b7491b15c283e9b
amed <amedduman@protonmail.com>
2022-07-06 13:03:30 +0300

    Minor fixes
    Add gravity to character controller, 
    Fix lighting, 
    Make rotate sensitivity serializable for character controller

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -6,6 +6,7 @@ public class PlayerEntity : MonoBehaviour
 {
     [SerializeField] InputActionReference _movement, _lookAround;
     [SerializeField] float _speed = .1f;
+    [SerializeField] float _turnSensitivity = .1f;
     CharacterController _controller;
 
     private void Start()
@@ -19,12 +20,12 @@ public class PlayerEntity : MonoBehaviour
         Vector2 movementInput = _movement.action.ReadValue<Vector2>();
         Vector3 moveDir = (transform.forward * movementInput.y) + 
         (transform.right * movementInput.x);
-        _controller.Move(moveDir * _speed);
+        _controller.SimpleMove(moveDir * _speed);
 
         Vector2 lookAroundInput = _lookAround.action.ReadValue<Vector2>();
         transform.rotation = Quaternion.Euler
         (0, 
-        transform.rotation.eulerAngles.y + lookAroundInput.x, 
+        (transform.rotation.eulerAngles.y + lookAroundInput.x * Time.deltaTime * _turnSensitivity), 
         0);
     }
 }
