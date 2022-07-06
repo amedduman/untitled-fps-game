1f613481b6e2ffb656c7d555e25075a183dc3d9f
amed <amedduman@protonmail.com>
2022-07-06 17:55:22 +0300

    Fix floating issue

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -7,6 +7,8 @@ public class PlayerEntity : MonoBehaviour
     [SerializeField] InputActionReference _movement, _lookAround;
     [SerializeField] float _speed = .1f;
     [SerializeField] float _turnSensitivity = .1f;
+    [SerializeField] float _gravity = 9.8f;
+    float _groundedGravity = .05f;
     CharacterController _controller;
 
     private void Start()
@@ -17,15 +19,43 @@ public class PlayerEntity : MonoBehaviour
 
     void Update()
     {
-        Vector2 movementInput = _movement.action.ReadValue<Vector2>();
-        Vector3 moveDir = (transform.forward * movementInput.y) + 
-        (transform.right * movementInput.x);
-        _controller.SimpleMove(moveDir * _speed);
+        Vector3 moveDir = Movement();
+        moveDir = Gravity(moveDir);
+        Rotation();
 
+        _controller.Move(moveDir * _speed * Time.deltaTime);
+    }
+
+    private Vector3 Movement()
+    {
+        Vector2 movementInput = _movement.action.ReadValue<Vector2>();
+        Vector3 moveDir = (transform.forward * movementInput.y) +
+        (transform.right * movementInput.x);
+        return moveDir;
+    }
+
+    private Vector3 Gravity(Vector3 moveDir)
+    {
+        Vector3 gravityDir = new Vector3(0, -1, 0);
+        if (_controller.isGrounded)
+        {
+            moveDir += gravityDir * _groundedGravity;
+
+        }
+        else
+        {
+            moveDir += gravityDir * _gravity;
+        }
+
+        return moveDir;
+    }
+
+    private void Rotation()
+    {
         Vector2 lookAroundInput = _lookAround.action.ReadValue<Vector2>();
         transform.rotation = Quaternion.Euler
-        (0, 
-        (transform.rotation.eulerAngles.y + lookAroundInput.x * Time.deltaTime * _turnSensitivity), 
+        (0,
+        (transform.rotation.eulerAngles.y + lookAroundInput.x * Time.deltaTime * _turnSensitivity),
         0);
     }
 }
