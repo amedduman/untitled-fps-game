b4f0c5dda632e28723354634d0a379eb2b1dbc31
amed <amedduman@protonmail.com>
2022-07-15 18:16:16 +0300

    Add feedbacks for enemy

--- Assets/Scripts/OtherEntities/DummyEntity.cs
+++ Assets/Scripts/OtherEntities/DummyEntity.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using DG.Tweening;
+using MoreMountains.Feedbacks;
 
 public class DummyEntity : MonoBehaviour, IDamageable
 {
     [SerializeField] [Min(0)] float _maxHealth = 100;
     [SerializeField] [Min(0)] float _damageImpactFactor = .1f;
     [SerializeField] Transform _model;
+    [Foldout("Feedbacks", true)]
+    [SerializeField] MMF_Player _onDamage;
+    [SerializeField] MMF_Player _onDeath;
     float _health;
     PlayerEntity _player;
     NavMeshAgent _agent;
@@ -32,20 +36,22 @@ public class DummyEntity : MonoBehaviour, IDamageable
     public void GetDamage(int damage)
     {
         if(_health <= 0) return;
+        _onDamage.PlayFeedbacks();
         _health -= damage;
         _health = Mathf.Clamp(_health, 0, _maxHealth);
         if(_health <= 0)
         {
+            _onDeath.PlayFeedbacks();
             _agent.enabled = false;
             GetComponent<Collider>().enabled = false;
             transform.DORotate(transform.rotation.eulerAngles + new Vector3(90, 0, 0), .5f).SetEase(Ease.InFlash);
         }
         else
         {
-            if(!DOTween.IsTweening(_model))
-            {
-                _model.DOPunchPosition(Vector3.up * _damageImpactFactor, _damageImpactFactor);
-            }
+            // if(!DOTween.IsTweening(_model))
+            // {
+            //     _model.DOPunchPosition(Vector3.up * _damageImpactFactor, _damageImpactFactor);
+            // }
         }
     }
 }
